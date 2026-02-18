1fb6f7dc073a081b5c7260561723c9dce7a8095a
mahes765 <fachvery@gmail.com>
2026-02-18 09:45:00 +0700

    Implemented Axum framework and configured Tokio runtime

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpListener;
+#[tokio::main]
+
+async fn main() {
+    let listener = TcpListener::bind("0.0.0.0:4000").await.unwrap();
 }
